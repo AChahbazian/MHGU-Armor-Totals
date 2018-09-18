726f6f1e30adaae52edbe003145580c8186fa78e
AChahbazian <36460872+AChahbazian@users.noreply.github.com>
2018-09-18 12:47:30 -0700

    Cleaned up table logic for accessing correct table

--- MHGU Armor Totals/MainWindow.xaml.cs
+++ MHGU Armor Totals/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using HtmlAgilityPack;
 
 namespace MHGU_Armor_Totals
@@ -43,25 +31,17 @@ namespace MHGU_Armor_Totals
                 return;
             }
 
-            // Get first row of the materials table contents.
-            //var table = doc.DocumentNode.SelectSingleNode("//table[contains(@class, 'table table-sm\') and .//th//text()[contains(., \'Lv\')]]").InnerHtml;
-            HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id=\"app\"]/div[2]/div[1]/div[2]/div[2]/div/div[5]/table");
-            HtmlNodeCollection rows = table.SelectNodes("tr");
-            HtmlNode firstRow = rows[1];
-
-            // Checking the raw data
-            lstMaterials.Items.Add(firstRow.InnerHtml);
-
-            /*
-            // Load data from item table
-            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table.table-sm"))
+            // Get table with material cost.
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/div[2]/div[1]/div[2]/div[2]/div/div[5]/table"))
             {
                 foreach (HtmlNode row in table.SelectNodes("tr"))
                 {
-                    lstMaterials.Items.Add(row.InnerText.Trim('\n', ' '));
+                    foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                    {
+                        lstMaterials.Items.Add(cell.InnerText);
+                    }
                 }
             }
-            */
         }
     }
 }
